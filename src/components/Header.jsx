a42aa14db1aa60b12361d34bb80298c049164826
Daniill <geniusmacmath@gmail.com>
2022-06-03 15:47:01 +0300

    10th lesson of react

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+	return (
+		<header className="header">
+        <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Apple_logo_grey.svg/1724px-Apple_logo_grey.svg.png" />
+      </header>
+		)
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+
+const Profile = () => {
+	return (
+		<div className="content">
+        <div>
+          <img className="banner" src="https://commons.com.ua/file/thumbnail/iceland_large.jpg" alt=""/>
+        </div>
+        <div>Ava + Description</div>
+        <div>My Posts
+          <div>New Post</div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+            <div>Post 3</div>
+          </div>
+        </div>
+      </div>)
+}
+
+export default Profile
